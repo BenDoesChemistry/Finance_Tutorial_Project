56117a96c0848eadf7e12a8481db2c4b89a39ec0
Ben Payton <bennyp2494@gmail.com>
2024-07-24 08:22:26 -0600

    Changed Values

--- Investment.py
+++ Investment.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 import Future_Value as fv
 
 def main():
+    Principle = 6000
+    Number_of_years = 30
+    Investment_Percent = 10
+    Added_per_quarter = 800
 
-    Number_of_years = 35
-
-    High_Interest_Savings = fv.Future_Value(895,5,250,Number_of_years,4)
+    Investment = fv.Future_Value(Principle,Investment_Percent,Added_per_quarter,Number_of_years,4)
+    High_Interest_Savings = fv.Future_Value(Principle,5,Added_per_quarter,Number_of_years,4)
 
     print(High_Interest_Savings.get_summary())
+    print(Investment.get_summary())
 
     plt.xlabel("Number of Years")
     plt.ylabel("Dollar Value")
@@ -15,7 +19,8 @@ def main():
 
 
     plt.plot(High_Interest_Savings.get_time_range(),High_Interest_Savings.get_future_values())
-    #plt.legend(["low_Addition,low_Interest","High_Addition,low_Interest","low_Addition,High_Interest","High_Addition,High_Interest","High_Savings"])
+    plt.plot(Investment.get_time_range(),Investment.get_future_values())
+    plt.legend(["High Interest Savings", "Investment " + str(Investment_Percent)+ "%"])
     plt.show()
     input()
 
