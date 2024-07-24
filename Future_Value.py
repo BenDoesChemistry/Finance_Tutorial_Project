a9e74920925452f4c8040ee313ac179b6f8be340
Ben Payton <bennyp2494@gmail.com>
2024-07-23 21:39:22 -0600

    Minimum Viable Product

--- Future_Value.py
+++ Future_Value.py
@@ -8,16 +8,42 @@ class Future_Value():
         self.Payment_per_period = Payment_per_period
         self.Number_periods = Number_periods
         self.Matches_per_period = Matches_per_period
-        self.Values = self.get_future_value_list()
-        pass
+        self.Time_List = [0]
+        self.Values = []
+        self._calc_values()
+        self.Future_value = self.get_future_value()
 
-    def get_future_value_list(self):
+    def _calc_values(self):
         accumulator = self.Principle_value
-        value_list = []
-        value_list.append(accumulator)
+        self.Values.append(accumulator)
         for i in range(self.Number_periods*self.Matches_per_period):
+            self.Time_List.append(i/self.Matches_per_period + 1/self.Matches_per_period)
             accumulator = accumulator + self.Payment_per_period
         # Apply interest for the interest period
             accumulator = accumulator * (1 + (self.Percent_increase / (100*self.Matches_per_period)))
-            value_list.append(accumulator)
-        return value_list
+            self.Values.append(round(accumulator,2))
+        
+    
+    def get_future_values(self):
+        return self.Values
+    
+    def get_future_value(self):
+        return self.Values[-1]
+    
+    def get_time_range(self):
+        return self.Time_List
+
+    def get_summary(self):
+        string = f"Starting at ${self.Principle_value} and adding ${self.Payment_per_period} per matching period with a {self.Percent_increase}% increase per year for {self.Number_periods} years you will have ${self.Future_value}."
+        return string
+    
+if __name__ == "__main__":
+    Number_of_years = 35
+
+    High_Interest_Savings = Future_Value(895,5,250,Number_of_years,4)
+
+    print(High_Interest_Savings.get_summary())
+
+    print(len(High_Interest_Savings.get_future_values()))
+    print(len(High_Interest_Savings.get_time_range()))
+    print(High_Interest_Savings.get_time_range())
--- Investment.py
+++ Investment.py
@@ -1,80 +1,21 @@
 import matplotlib.pyplot as plt
-# future value function calculates the future value of an investment over a number of years.
-def future_value(Principle_value:float,Percent_Increase:float,Payment_Per_Period:float,Number_Periods:int,Matches_Per_Period:int) -> float:
-    # A variable to modify as we move through the years
-    Accumulator = Principle_value
-    # This loop occurs for the total number of payment periods.
-    for i in range(Number_Periods*Matches_Per_Period):
-        # Add the amount you would add per payment period
-        Accumulator = Accumulator + Payment_Per_Period
-        # Apply interest for the interest period
-        Accumulator = Accumulator * (1 + (Percent_Increase / (100*Matches_Per_Period)))
-    # Return the  future value
-    return round(Accumulator,2)
-
-def future_value_list(Principle_value:float,Percent_Increase:float,Payment_Per_Period:float,Number_Periods:int,Matches_Per_Period:int):
-    Accumulator = Principle_value
-    Val_List = [[],[]]
-    Val_List[0].append(0)
-    Val_List[1].append(Accumulator)
-    for i in range(Number_Periods*Matches_Per_Period):
-        # Add the amount you would add per payment period
-        Accumulator = Accumulator + Payment_Per_Period
-        # Apply interest for the interest period
-        Accumulator = Accumulator * (1 + (Percent_Increase / (100*Matches_Per_Period)))
-        Val_List[0].append((i+1)/Matches_Per_Period)
-        Val_List[1].append(round(Accumulator,2))
-    return Val_List
-
-#def Mults_CSV(Principle_value:float,Percent_Increase,Payment_Per_Period:float,Number_Periods:int,Matches_Per_Period:int):
-    #for i in Percent_Increase:
-
-def List_to_CSV(List_Thing,File_Name):
-    string = ""
-    template_string = "{},{}\n"
-    for i in range(len(List_Thing[0])):
-        string = string + template_string.format(List_Thing[0][i],List_Thing[1][i])
-
-    with open(File_Name,"w") as file:
-        file.write(string)
-
-
-# sumarize_future_value in a clean print function
-def summarize_future_value(Principle_value:float,Percent_Increase:float,Payment_Per_Period:float,Number_Periods:int,Matches_Per_Period:int) -> None:
-    Future_Value = future_value(Principle_value,Percent_Increase,Payment_Per_Period,Number_Periods,Matches_Per_Period)
-    True_Percent = Percent_Increase/100
-    string = f"Starting at ${Principle_value} and adding ${Payment_Per_Period} per matching period with a {True_Percent}% increase per year you will have ${Future_Value} after {Number_Periods} years.\n"
-    print(string)
+import Future_Value as fv
 
 def main():
-    print("High interest savings:")
-    summarize_future_value(100000,5,0,10,4)
-    print("Investing:")
-    summarize_future_value(100000,8,0,10,4)
-    print("High Yield Investing:")
-    summarize_future_value(100000,10,0,10,4)
-    Num_Years = 35
-    Save = future_value_list(890.49,3.95,200,Num_Years,4)
-    High_Save = future_value_list(890.49,5,1750,Num_Years,4)
-    tInvest = future_value_list(890.49,3.95,1750,Num_Years,4)
-    Invest = future_value_list(890.49,8,200,Num_Years,4)
-    High_Invest = future_value_list(890.49,8,1750,Num_Years,4)
 
-    #List_to_CSV(Save,"Collect_All.csv")
-    #List_to_CSV(Invest,"Invest.csv")
-    #List_to_CSV(High_Invest,"High_Invest.csv")
+    Number_of_years = 35
+
+    High_Interest_Savings = fv.Future_Value(895,5,250,Number_of_years,4)
+
+    print(High_Interest_Savings.get_summary())
 
     plt.xlabel("Number of Years")
     plt.ylabel("Dollar Value")
     plt.ticklabel_format(useOffset=False,style="plain")
 
 
-    plt.plot(tInvest[0],tInvest[1])
-    plt.plot(Save[0],Save[1])
-    plt.plot(Invest[0],Invest[1])
-    plt.plot(High_Invest[0],High_Invest[1])
-    plt.plot(High_Save[0],High_Save[1])
-    plt.legend(["low_Addition,low_Interest","High_Addition,low_Interest","low_Addition,High_Interest","High_Addition,High_Interest","High_Savings"])
+    plt.plot(High_Interest_Savings.get_time_range(),High_Interest_Savings.get_future_values())
+    #plt.legend(["low_Addition,low_Interest","High_Addition,low_Interest","low_Addition,High_Interest","High_Addition,High_Interest","High_Savings"])
     plt.show()
     input()
 
